a216ca0b0b2c0c61f4e902caf8c752f1b7f127b9
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-16 22:59:11 +0100

    RM-1 Create MainActivity class and layout

---
+++ app/src/main/java/ua/com/sevenpowerx/android/remind_meproject/MainActivity.java
@@ -0,0 +1,17 @@
+package ua.com.sevenpowerx.android.remind_meproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by splaa User notebook acer  on 16.12.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
